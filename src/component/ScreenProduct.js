86029c84897467ec75ace075d6debacea57d5673
sandy13oct1992 <sandy13oct1992@gmail.com>
2024-04-05 19:20:58 +0530

    Starting E-commerce project

---
+++ src/component/ScreenProduct.js
@@ -0,0 +1,60 @@
+
+const productsArr = [
+
+    {
+    
+    title: 'Colors',
+    
+    price: 100,
+    
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+    
+    },
+    
+    {
+    
+    title: 'Black and white Colors',
+    
+    price: 50,
+    
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+    
+    },
+    
+    {
+    
+    title: 'Yellow and Black Colors',
+    
+    price: 70,
+    
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+    
+    },
+    
+    {
+    
+    title: 'Blue Color',
+    
+    price: 100,
+    
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
+    
+    }
+    
+    ]
+    
+    
+const ScreenProduct = () => {
+
+return(
+  productsArr.map((items) => (
+    <div key={items.imageUrl}>
+      <h2>{items.title}</h2>
+     
+      <img src={items.imageUrl} />
+      <h3>${items.price}</h3>
+    </div>  
+  ))
+)
+}
+export default ScreenProduct;
